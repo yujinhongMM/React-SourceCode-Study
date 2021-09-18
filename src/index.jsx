48b2ba5714412a5c8c8f9964daf808c97bb58b44
yujinhong01 <yujinhong01@58.com>
2021-09-18 17:02:52 +0800

    反向继承

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,61 @@
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const withLoading = (message) => (OldComponent) => {
-  return class extends React.Component {
+/**
+ * 比如说我们有一个组件是第三房提供的，我们不能改，也不能继承，但是还是想做一些修改或者说增强
+ */
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    //此处的this就是子类的实例
+    this.state = {name: '章三'}
+    console.log('Button constructor')
+  }
+  
+  componentDidMount() {
+    console.log('Button componentDidMount');
+  }
+  render() {
+    console.log('Button render');
+    return <button name={this.state.name} title={this.props.title} />
+  }
+}
+
+const wrapper = OldComponent => {
+  return class extends OldComponent {
+    constructor(props) {
+      super(props);
+      console.log(this.state);
+      this.state = {number: 0};
+      console.log('wrapper constructor')
+    }
+
+    handleClick = () => {
+      this.setState({number:this.state.number + 1});
+    }
+
+    componentDidMount() {
+      console.log('wrapper componentDidMount');
+      super.componentDidMount();
+    }
+    
     render() {
-      const state = {
-        show: () => {
-          let div = document.createElement('div');
-          div.innerHTML = `<p id="loading" style="position:absolute;top:100px;z-index:100;background-color:red;">${message}</p>`
-          document.body.appendChild(div);
-        },
-        hide: () => {
-          document.getElementById('loading').remove();
-        }
+      console.log("wrapper render");
+      // 调用父亲=类的render方法，返回一个虚拟DOM
+      let renderVdom = super.render(); // <button name={this.state.name} title={this.props.title} />
+      let newProps = {
+        ...renderVdom.props, // {name:undefined, title:'标题'} name为undefiend是因为子类的this.state从{name: '章三'}变成了{number: 0};
+        ...this.state, // {number: 0}
+        onClick: this.handleClick
       }
-      return <OldComponent {...this.props} {...state} />
+      return React.cloneElement(renderVdom, newProps, this.state.number, 'ok');// renderVdom老的属性对象，newProps新的属性对象，this.state.number：儿子
     }
   }
 }
+let WrappedButton = wrapper(Button);
 
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.title}
-        <button onClick={this.props.show}>show</button>
-        <button onClick={this.props.hide}>hide</button>
-      </div>
-    )
-  }
-}
+ReactDOM.render(<WrappedButton title="标题"/>, document.getElementById('root'));
 
-let HelloWithLoading = withLoading('加载中......')(Hello);
-
-ReactDOM.render(<HelloWithLoading title="标题"/>, document.getElementById('root'));
+/**
+ * super
+ */
