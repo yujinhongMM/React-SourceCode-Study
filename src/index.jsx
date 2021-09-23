28d2d944cbf0d6d48c70a83f4d93b6d629437510
yujinhong01 <yujinhong01@58.com>
2021-09-23 16:30:33 +0800

    改成高阶组件

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,37 @@
 import React from './react';
 import ReactDOM from './react-dom';
-class MouseTracker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {x:0, y:0};
-  }
-  handleMouseMove = (event) => {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY
-    })
-  }
-  render() {
-    let render = typeof this.props.render === 'function' ? this.props.render : this.props.children;
-    return (
-      <div onMouseMove={this.handleMouseMove}>
-        {render(this.state)}
-      </div>
-    )
+
+function withTracker(OldComponent) {
+  return class MouseTracker extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {x:0, y:0};
+    }
+    handleMouseMove = (event) => {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      })
+    }
+    render() {
+      return (
+        <div onMouseMove={this.handleMouseMove}>
+          <OldComponent {...this.state} />
+        </div>
+      )
+    }
   }
 }
-// render props 组件的属性是一个render方法，返回值是要渲染的React元素
-ReactDOM.render(<MouseTracker>
-  {
-    (props) => (
-      <React.Fragment>
-        <h1>移动鼠标</h1>
-        <p>当前鼠标的位置是{props.x},{props.y}</p>
-      </React.Fragment>
-    )
-  }
-</MouseTracker>, document.getElementById('root'));
+
+
+function Show(props) {
+  return (
+    <React.Fragment>
+      <h1>移动鼠标</h1>
+      <p>当前鼠标的位置是{props.x},{props.y}</p>
+    </React.Fragment>
+  )
+}
+
+let HighOrderShow = withTracker(Show);
+ReactDOM.render(<HighOrderShow />, document.getElementById('root'));
