9d38dd009f5bc55847f98801f1bb8685385ba172
yujinhong01 <yujinhong01@58.com>
2021-09-23 16:24:19 +0800

    放在props.children中做兼容

--- src/index.jsx
+++ src/index.jsx
@@ -12,19 +12,22 @@ class MouseTracker extends React.Component {
     })
   }
   render() {
+    let render = typeof this.props.render === 'function' ? this.props.render : this.props.children;
     return (
       <div onMouseMove={this.handleMouseMove}>
-        {this.props.render(this.state)}
+        {render(this.state)}
       </div>
     )
   }
 }
 // render props 组件的属性是一个render方法，返回值是要渲染的React元素
-ReactDOM.render(<MouseTracker render={
-  (props) => (
-    <React.Fragment>
-      <h1>移动鼠标</h1>
-      <p>当前鼠标的位置是{props.x},{props.y}</p>
-    </React.Fragment>
-  )
-}></MouseTracker>, document.getElementById('root'));
+ReactDOM.render(<MouseTracker>
+  {
+    (props) => (
+      <React.Fragment>
+        <h1>移动鼠标</h1>
+        <p>当前鼠标的位置是{props.x},{props.y}</p>
+      </React.Fragment>
+    )
+  }
+</MouseTracker>, document.getElementById('root'));
