2d9e88a7b210dd425de271cca7055f80c8e2352f
yujinhong01 <yujinhong01@58.com>
2022-03-21 12:14:57 +0800

    删除文件

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override, addBabelPlugin } = require('customize-cra');
-module.exports = override(
-    addBabelPlugin(
-        [
-            "@babel/plugin-proposal-decorators", { legacy: true }
-        ]
-    )
-)
